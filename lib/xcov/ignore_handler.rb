9fbb2e86d5beba95e5e460fc49c8ef78d5eb0fd6
Carlos Vidal <nakioparkour@gmail.com>
2016-03-20 19:28:51 +0000

    added ignore_file_path option

--- lib/xcov/ignore_handler.rb
+++ lib/xcov/ignore_handler.rb
@@ -4,11 +4,12 @@ module Xcov
 
     def read_ignore_file
       require "yaml"
+      ignore_file_path = Xcov.config[:ignore_file_path]
       ignore_list = []
       begin
-        ignore_list = YAML.load_file(".xcovignore")
+        ignore_list = YAML.load_file(ignore_file_path)
       rescue
-        Helper.log.info "Skipping file blacklisting as no .xcovignore file was found".yellow
+        Helper.log.info "Skipping file blacklisting as no ignore file was found at path #{ignore_file_path}".yellow
       end
 
       ignore_list

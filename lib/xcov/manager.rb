eec82bfd4272c3a2abb34425ee8c13f86566ba4b
Carlos Vidal <nakioparkour@gmail.com>
2016-04-09 17:11:50 +0100

    ingore_handler property on Xcov instance

--- lib/xcov/manager.rb
+++ lib/xcov/manager.rb
@@ -11,8 +11,8 @@ module Xcov
       FastlaneCore::Project.detect_projects(options)
       Xcov.project = FastlaneCore::Project.new(options)
 
-      # Set ignored files list
-      Xcov.ignore_list = IgnoreHandler.new.read_ignore_file
+      # Set ignored files handler
+      Xcov.ignore_handler = IgnoreHandler.new
 
       # Print summary
       FastlaneCore::PrintTable.print_values(config: options, hide_keys: [:slack_url], title: "Summary for xcov #{Xcov::VERSION}")

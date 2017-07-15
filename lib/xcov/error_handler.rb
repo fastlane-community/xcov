06431241787a60042cb23aaea1e749792fb99019
Carlos Vidal <nakioparkour@gmail.com>
2017-07-15 14:00:05 +0100

    bumped version and updated changelog

--- lib/xcov/error_handler.rb
+++ lib/xcov/error_handler.rb
@@ -1,3 +1,4 @@
+
 module Xcov
   class ErrorHandler
 
--- lib/xcov/version.rb
+++ lib/xcov/version.rb
@@ -1,6 +1,6 @@
 module Xcov
 
-  VERSION = "1.2.0"
+  VERSION = "1.3.0"
   DESCRIPTION = "xcov is a friendly visualizer for Xcode's code coverage files"
 
 end

83f02dc522f7a500d7aa358743c194fbb5ad9e09
Lonny Antunes <lonny.antunes@gmail.com>
2019-09-05 15:39:42 +0200

    Updated the run method to get the json_report value

--- lib/xcov/manager.rb
+++ lib/xcov/manager.rb
@@ -33,6 +33,8 @@ module Xcov
       submit_to_coveralls(report)
       tmp_dir = File.join(Xcov.config[:output_directory], 'tmp')
       FileUtils.rm_rf(tmp_dir) if File.directory?(tmp_dir)
+
+      json_report
     end
 
     def parse_xccoverage
--- lib/xcov/version.rb
+++ lib/xcov/version.rb
@@ -1,6 +1,6 @@
 module Xcov
 
-  VERSION = "1.5.1"
+  VERSION = "1.5.2"
   DESCRIPTION = "xcov is a friendly visualizer for Xcode's code coverage files"
 
 end
